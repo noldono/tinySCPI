49ac3ac57a1d184f520b19e60018b9d18447a7e6
Heesang Han <heesang@vt.edu>
2023-08-30 21:02:40 -0400

    more unit tests

--- tinyscpi/tests/test_scpi_parser.py
+++ tinyscpi/tests/test_scpi_parser.py
@@ -33,6 +33,9 @@ class ParserTestCase(unittest.TestCase):
         self.assertEqual(self.parser.parseCommand('BAND:RES 3'), ('BAND:RES', [3]))
         self.assertEqual(self.parser.parseCommand('BAND:RES 600'), ('BAND:RES', [600]))
 
+        # TODO: Alias Testing, this line currently fails
+        self.assertEqual(self.parser.parseCommand('BAND:RESolution 3'), ('BAND:RES', [3]))
+
         with self.assertRaises(Exception):
             self.parser.parseCommand('BANDRES')
 
@@ -55,6 +58,61 @@ class ParserTestCase(unittest.TestCase):
 
     def testParseCommand_BAND_RES_AUTO(self):
         self.assertEqual(self.parser.parseCommand('BAND:RES:AUTO ON'), ('BAND:RES:AUTO', []))
+        self.assertEqual(self.parser.parseCommand('BAND:RES:AUTO OFF'), ('BAND:RES:AUTO', []))
+
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('BAND:RES:AUTO')
+
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('BAND:RES:AUTO auto')
+
+    def testParseCommand_SYST_VERS(self):
+        self.assertEqual(self.parser.parseCommand('SYST:VERS?'), ('SYST:VERS?', []))
+
+        # TODO: Alias Testing, this line currently fails
+        self.assertEqual(self.parser.parseCommand('SYSTem:VERSion?'), ('SYST:VERS?', []))
+
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('SYST:VERS')
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('SYST:VERS? 1')
+
+
+    def testParseCommand_AMPL_ATT(self):
+        self.assertEqual(self.parser.parseCommand('AMPL:ATT 0'), ('AMPL:ATT', [0]))
+        self.assertEqual(self.parser.parseCommand('AMPL:ATT 31'), ('AMPL:ATT', [31]))
+
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('AMPL:ATT 32')
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('AMPL:ATT -1')
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('AMPL:ATT 0.2')
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('AMPL:ATT')
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('AMPL:ATT 0 1')
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('AMPL:ATT std')
+
+        # TODO: Alias Testing, this line currently fails
+        self.assertEqual(self.parser.parseCommand('AMPLitude:ATTenuation 0'), ('AMPL:ATT', [0]))
+
+    def testParseCommand_AMPL_ATT_AUTO(self):
+        self.assertEqual(self.parser.parseCommand('AMPL:ATT:AUTO'), ('AMPL:ATT:AUTO', []))
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('AMPL_ATT_AUTO On')
+        with self.assertRaises(Exception):
+            self.parser.parseCommand('AMPL_ATT_AUT')
+
+        # TODO: Alias Testing, this line currently fails
+        self.assertEqual(self.parser.parseCommand('AMPLitude:ATTenuation:AUTO'), ('AMPL:ATT:AUTO', []))
+
+    def testParseCommand_SOUR_POW_LEV_IMM_AMPL(self):
+        self.assertEqual(self.parser.parseCommand('SOURce:POWer:LEVel:IMMediate:AMPLitude OFF'), ('SOUR:POW:LEV:IMM:AMPL', ['off']))
+        self.assertEqual(self.parser.parseCommand('SOURce:POWer:LEVel:IMMediate:AMPLitude 30'), ('SOUR:POW:LEV:IMM:AMPL', ['30']))
+
+
 
     def testParseResult(self):
         self.assertEqual(1, 1)
