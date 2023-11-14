842f12f62799f88ea697914cf1f7691ad4614c4d
Nolan Donovan <rndonovan@vt.edu>
2023-11-14 09:15:59 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = '1.0.1'
 DESCRIPTION = 'An interface that enables the use of SCPI commands for the tinySA'
 LONG_DESCRIPTION = ''
 
@@ -11,6 +15,7 @@ setup(
     author="tinySCPI Team",
     author_email="<rndonovan1@gmail.com>",
     description=DESCRIPTION,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=['numpy', 'pillow', 'wheel', 'setuptools', 'pyserial'],
